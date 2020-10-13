9fb42a9f3412280c46b8ae5c4e800b044cb26957
Guaramma <61195555+Guaramma@users.noreply.github.com>
2020-10-14 00:34:18 +0300

    Vote improvment

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -14,11 +14,10 @@ import {
   Root,
   UseMiddleware,
 } from "type-graphql";
-import { MyContext } from "src/types";
+import { MyContext } from "../types";
 import { isAuth } from "../middleware/isAuth";
 import { getConnection } from "typeorm";
 import { Updoot } from "../entities/Updoot";
-import { tmpdir } from "os";
 
 @InputType()
 class PostInput {
@@ -39,8 +38,8 @@ class PaginatedPosts {
 @Resolver(Post)
 export class PostResolver {
   @FieldResolver(() => String)
-  textSnippet(@Root() root: Post) {
-    return root.text.slice(0, 50);
+  textSnippet(@Root() post: Post) {
+    return post.text.slice(0, 50);
   }
 
   @Mutation(() => Boolean)
@@ -55,16 +54,17 @@ export class PostResolver {
     const { userId } = req.session;
 
     const updoot = await Updoot.findOne({ where: { postId, userId } });
-    //the user has voted on the post before
-    // and they are changig their vote
+
+    // the user has voted on the post before
+    // and they are changing their vote
     if (updoot && updoot.value !== realValue) {
       await getConnection().transaction(async (tm) => {
         await tm.query(
           `
-          update updoot
-          set value = $1
-          where "postId" = $2 and "userId" = $3
-              `,
+    update updoot
+    set value = $1
+    where "postId" = $2 and "userId" = $3
+        `,
           [realValue, postId, userId]
         );
 
@@ -82,18 +82,18 @@ export class PostResolver {
       await getConnection().transaction(async (tm) => {
         await tm.query(
           `
-        insert into updoot ("userId", "postId", value)
-        values ($1,$2,$3);
+    insert into updoot ("userId", "postId", value)
+    values ($1, $2, $3)
         `,
           [userId, postId, realValue]
         );
 
         await tm.query(
           `
-        update post
-        set points = points + $1
-        where id = $2;
-        `,
+    update post
+    set points = points + $1
+    where id = $2
+      `,
           [realValue, postId]
         );
       });
@@ -104,30 +104,42 @@ export class PostResolver {
   @Query(() => PaginatedPosts)
   async posts(
     @Arg("limit", () => Int) limit: number,
-    @Arg("cursor", () => String, { nullable: true }) cursor: string | null
+    @Arg("cursor", () => String, { nullable: true }) cursor: string | null,
+    @Ctx() { req }: MyContext
   ): Promise<PaginatedPosts> {
     const realLimit = Math.min(50, limit);
     const reaLimitPlusOne = realLimit + 1;
 
-    const replacements: any = [reaLimitPlusOne];
+    const replacements: any[] = [reaLimitPlusOne];
 
+    if (req.session.userId) {
+      replacements.push(req.session.userId);
+    }
+
+    let cursorIdx = 3;
     if (cursor) {
       replacements.push(new Date(parseInt(cursor)));
+      cursorIdx = replacements.length;
     }
 
     const posts = await getConnection().query(
       `
-    select p.*, 
+    select p.*,
     json_build_object(
       'id', u.id,
       'username', u.username,
       'email', u.email,
       'createdAt', u."createdAt",
       'updatedAt', u."updatedAt"
-      ) creator
+      ) creator,
+    ${
+      req.session.userId
+        ? '(select value from updoot where "userId" = $2 and "postId" = p.id) "voteStatus"'
+        : 'null as "voteStatus"'
+    }
     from post p
     inner join public.user u on u.id = p."creatorId"
-    ${cursor ? `where p."createdAt" < $2` : ""}
+    ${cursor ? `where p."createdAt" < $${cursorIdx}` : ""}
     order by p."createdAt" DESC
     limit $1
     `,
@@ -156,8 +168,8 @@ export class PostResolver {
   }
 
   @Query(() => Post, { nullable: true })
-  post(@Arg("id") id: number): Promise<Post | undefined> {
-    return Post.findOne(id);
+  post(@Arg("id", () => Int) id: number): Promise<Post | undefined> {
+    return Post.findOne(id, { relations: ["creator"] });
   }
 
   @Mutation(() => Post)
